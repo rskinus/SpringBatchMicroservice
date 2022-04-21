e82be5f85e98d122e29f9d3961607560b69ea7d0
Satishkumar Ragunathan <satishkumar@Satishkumars-MacBook-Air.local>
2022-04-21 13:25:36 -0500

    Initial Commit

---
+++ src/test/java/com/satish/learning/SpringBatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.learning.SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
